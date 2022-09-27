6300f7846ff2e3657c6569a3f4efe30462c59e41
MatheusOliveira1996 <matheus.hortolandia@gmail.com>
2022-09-26 23:34:49 -0300

    Add project files.

---
+++ Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace UpcastingDowncasting.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBAlance ()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
